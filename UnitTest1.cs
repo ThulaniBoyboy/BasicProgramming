38f5106fea3019ca5aa302481de8cf704c176c17
ThulaniBoyboy <43739158+ThulaniBoyboy@users.noreply.github.com>
2018-10-18 14:40:48 +0200

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -12,7 +12,7 @@ namespace BasicProgrammingTests
         {
             string expected = "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(400, 'Manmay', 'Mohanty', 'M', '07/03/1983', True)\r\ninsert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(401, 'Marike', 'Fourie', 'F', '30/11/2001', False)\r\n";
 
-            Program.ConvertFormat1ToSql("c:/users/user/desktop/test.txt", 400);
+            Program.ConvertFormat1ToSql("test.txt", 400);
             using (StreamReader sr = new StreamReader(File.OpenRead("c:/users/user/desktop/test.sql")))
             {
                 string actual = sr.ReadToEnd();
@@ -29,7 +29,7 @@ namespace BasicProgrammingTests
         {
             string expected = "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(400, 'Manmay', 'Mohanty', 'M', '07/03/1983', True)\r\ninsert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(401, 'Marike', 'Fourie', 'F', '30/11/2001', False)\r\n";
 
-            Program.ConvertFormat2ToSql("c:/users/user/desktop/test1.txt", 400);
+            Program.ConvertFormat2ToSql("test1.txt", 400);
             using (StreamReader sr = new StreamReader(File.OpenRead("c:/users/user/desktop/test1.sql")))
             {
                 string actual = sr.ReadToEnd();
@@ -45,7 +45,7 @@ namespace BasicProgrammingTests
         {
             string expected = "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(400, 'Manmay', 'Mohanty', 'M', '07/03/1983', True)\r\ninsert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(401, 'Marike', 'Fourie', 'F', '30/11/2001', False)\r\n";
 
-            Program.ConvertFormat3ToSql("c:/users/user/desktop/test2.txt", 400);
+            Program.ConvertFormat3ToSql("test2.txt", 400);
             using (StreamReader sr = new StreamReader(File.OpenRead("c:/users/user/desktop/test2.sql")))
             {
                 string actual = sr.ReadToEnd();
